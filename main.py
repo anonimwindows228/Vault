2239daa2a2e38ccf0976bc8a6c9333210497471d
Maksim Stepnov <nigrillapidrilla@internet.ru>
2026-04-06 17:31:52 -0700

    Added support for cmd args and windows registry

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+import sys
 from ui import VaultApp
 
+_FLAG_TO_TAB = {
+    "--encrypt":    0,
+    "--decrypt":    1,
+    "--compress":   2,
+    "--decompress": 3,
+}
+
 if __name__ == "__main__":
-    app = VaultApp()
+    flag     = sys.argv[1] if len(sys.argv) > 1 else None
+    filepath = sys.argv[2] if len(sys.argv) > 2 else None
+
+    tab = _FLAG_TO_TAB.get(flag)
+
+    app = VaultApp(startup_tab=tab, startup_file=filepath)
     app.mainloop()
